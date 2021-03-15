1df6ffa5a8ddcbbed504e3e2b2dc449dfecc89e6
stm37 <79359895+stm37@users.noreply.github.com>
2021-03-15 12:22:08 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'My Owner BOT', m)
+  // this.sendContact(m.chat, '62895605030379', 'junaedi', m)
+  this.sendContact(m.chat, '0895605030379', 'junaedi', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
